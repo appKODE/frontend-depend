1f1badd5db65e90fe730499f08466c1b434daee0
Maxim Tereshko <hey@tereshko.net>
2024-05-02 21:39:21 +0200

    chore: fixed tsup configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ['packages/**'],
+  ignorePatterns: ['packages/**', 'internal/**'],
   extends: ['@repo/config-eslint/library.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
