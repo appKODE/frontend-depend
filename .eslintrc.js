5339fa1ce1c15d48439409924181a143cba1229f
Maxim Tereshko <hey@tereshko.net>
2024-05-03 11:32:20 +0200

    fix(build): fixed typescript config (#34)
    * fix: refactored internal config packages
    
    * fix: fixed package.json linting
    
    * chore: fixed tsup configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ['packages/**'],
-  extends: ['@repo/eslint-config-internal/library.js'],
+  ignorePatterns: ['packages/**', 'internal/**'],
+  extends: ['@repo/config-eslint/library.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
